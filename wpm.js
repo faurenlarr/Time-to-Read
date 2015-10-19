9808494f23b88e7045e5776003da6fabf293650d
Lauren Farr <laurenfarr@Lauren-MacBook-Pro.local>
2015-10-19 15:28:52 -0400

    Time to Read equation JS oct19

---
+++ wpm.js
@@ -0,0 +1,31 @@
+
+
+// var wpm = prompt("How many words can you read per minute?");
+// var getTimeToRead = function() {
+//   var str = document.querySelector('p').innerHTML;
+//   var str2 = str.split(" ");
+//   var wordCount = str2.length;
+//   var total = Math.round(wordCount/wpm);
+//   return total + " minutes";
+// };
+//
+// document.querySelector('h3').innerHTML+=getTimeToRead();
+
+var wpm = prompt("How many words can you read per minute?");
+var getTimeToRead = function() {
+  var myDomArray = [].slice.call(document.querySelectorAll('p'));
+  var newArray = " ";
+  myDomArray.forEach(function(currItem,idx, arr) {
+  console.log(currItem.textContent) ;
+  newArray += currItem.textContent;
+
+});
+
+  var actualArray = newArray.split(" ");
+  var wordCount = actualArray.length;
+
+  var total = Math.round(wordCount/wpm);
+  return total + " minutes";
+};
+ getTimeToRead();
+document.querySelector('h3').innerHTML += getTimeToRead();
